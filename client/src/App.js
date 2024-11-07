7f78a46084eb13b425732abde6df2a2569477b45
JULIA KLODA <19019705@uhi.ac.uk>
2024-11-07 18:45:24 +0000

    Made a request to the Flask server using fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return <div className="App">Hello, world!</div>;
+  const [data, setData] = useState(null);
+
+useEffect(() => {
+  fetch('http://127.0.0.1:5000/api/data')
+      .then(response => response.json())
+      .then(data => setData(data.message))
+      .catch(error => console.error("Error fetching data:", error));
+}, []);
+
+return (
+  <div className="App">
+      <h1>{data || "Loading..."}</h1>
+  </div>
+);
 }
+
 export default App;
