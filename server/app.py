3c6bfec0a5c50bf6fbd376be9c3ec83c29789cd4
Martin Cameron <mcameron.brix@gmail.com>
2024-11-28 00:20:14 +0000

    added login manager and session to app

--- server/app.py
+++ server/app.py
@@ -1,12 +1,41 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, session
 from flask_cors import CORS
+import os
+import mysql.connector.pooling
+from flask_login import LoginManager
+from dotenv import load_dotenv
+
+load_dotenv()
+login_manager = LoginManager()
 
 app = Flask(__name__)
+login_manager.init_app(app)
 CORS(app, origins=["http://localhost:3000"])
+
+connection_pool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name="moviefinder_pool",
+    user=os.getenv('USER'),
+    host=os.getenv('HOST'),
+    password=os.getenv('PASS'),
+    database=os.getenv('DATABASE')
+)
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
     return jsonify({"message": "Hello from Flask with CORS!"})
+
+@app.route('/api/getuser', methods=['GET'])
+def get_user():
+    try:
+        connection = connection_pool.get_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = 'username'")
+        details = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return jsonify({"details": details})
+    except:
+        return jsonify({"message": "error"})
 
 if __name__ == '__main__':
     app.run(debug=True)
