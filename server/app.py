f4ed649d833d7dd2ca00d2ff14190b9b922e4a3c
Martin Cameron <mcameron.brix@gmail.com>
2024-11-29 19:53:47 +0000

    added register route and refactored existing route for security and json requests

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, session, request
 from flask_cors import CORS
 import os
+import re
 import mysql.connector.pooling
+from passlib.hash import sha256_crypt
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 from dotenv import load_dotenv
 
@@ -52,19 +54,20 @@ def loader_user(user_id):
 def get_data():
     return jsonify({"message": "Hello from Flask with CORS!"}), 200
 
-@app.route('/api/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
 def get_user():
     if request.method == 'POST':
         try:
-            username = request.form.get('username')
-            password = request.form.get('password')
+            result = request.get_json(silent=True)
+            username = request.form.get('username') or result['username']
+            password = request.form.get('password') or result['password']
             connection = connection_pool.get_connection()
             cursor = connection.cursor(dictionary=True)
             cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
             account = cursor.fetchone()
             cursor.close()
             connection.close()
-            if account['password'] == password:
+            if sha256_crypt.verify(password, account['password']):
                 user = User(account)
                 login_user(user)
                 return jsonify({'message': 'successfully logged in'}), 200
@@ -73,7 +76,44 @@ def get_user():
         except:
             return jsonify({"message": "error handling request"}), 400
 
-@app.route('/api/logout')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    result = request.get_json(silent=True)
+    if request.method == 'POST' and 'username' in request.form or result and 'password' in request.form or result and 'email' in request.form or result:
+        try:
+            
+            username = request.form.get('username') or result['username']
+            password = request.form.get('password') or result['password']
+            email = request.form.get('email') or result['email']
+
+            if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                return jsonify({'message': 'incorrectly formatted email'}), 400
+            elif not re.match(r'[a-zA-z0-9]+', username):
+                return jsonify({'message': 'incorrectly formatted username characters must be a-z, A-z, 0-9'}), 400
+            
+            connection = connection_pool.get_connection()
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute('SELECT username FROM users WHERE username = %s;', (username,))
+            is_username = cursor.fetchone()
+            cursor.execute('SELECT email FROM users WHERE email = %s;', (email,))
+            is_email = cursor.fetchone()
+
+            if is_username:
+                return jsonify({"message": "username already exists"})
+            if is_email:
+                return jsonify({"message": "email already exists"})
+            else:
+                password = sha256_crypt.hash(password)
+                cursor.execute('INSERT INTO users (username, password, email) VALUES (%s, %s, %s);', (username, password, email))
+                connection.commit()
+            return jsonify({'message': 'successfully created account'}), 200
+        except Exception as e: 
+            return jsonify({'message': f'error unknown server error {e}'}), 500
+
+    else:
+        return jsonify({'message': 'please check http request body, username, email, password are missing or method not POST '}), 400 
+
+@app.route('/logout')
 def logout():
     logout_user()
     return jsonify({"message": "successfully logged out"}), 200
