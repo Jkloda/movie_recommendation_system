c7e0e21c6961145cf7edd4499f3fc69882f3418f
JULIA KLODA <19019705@uhi.ac.uk>
2024-11-29 17:19:33 +0000

    added login request component, fixed url

--- client/src/components/LoginService.js
+++ client/src/components/LoginService.js
@@ -1,21 +1,26 @@
 export const loginUser = async (credentials) => {
-    try {
-      const response = await fetch("http://localhost:3000/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(credentials),
-      });
-  
-      if (!response.ok) {
-        throw new Error("Failed to login");
-      }
-  
-      return await response.json();
-    } catch (error) {
-      console.error("Login error:", error);
-      throw error;
+  const url = "http://127.0.0.1:5000";
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // Include credentials (cookies, HTTP authentication, etc.)
+      body: JSON.stringify(credentials), // Convert the credentials object to a JSON string
+    });
+
+    if (!response.ok) {
+      const errorDetails = await response.json();
+      throw new Error(errorDetails.message || "Failed to login");
     }
-  };
-  
+
+    const result = await response.json();
+    console.log("Login successful:", result);
+    return result;
+  } catch (error) {
+    console.error("Login error:", error.message);
+    throw error;
+  }
+};
