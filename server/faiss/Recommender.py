554ac30d15cc3af5e585ae088c9ed76b772cc1c1
Martin Cameron <highlandrocketofficial@gmail.com>
2025-02-24 18:26:54 +0000

    created class to handle the RAG between index and llama3.2

--- server/faiss/Recommender.py
+++ server/faiss/Recommender.py
@@ -1,5 +1,22 @@
-
+from Indexer import Indexer
+from Http import HttpLayer
+import asyncio
 
 class Recommender:
     def __init__(self):
-        
+        self.recommendation = [] 
+        self.httpLayer = HttpLayer()
+        self.indexer = Indexer()
+
+    def search_index(self, favourites):
+        total_movies = 1
+        #len(favourites)
+        index_results = self.indexer.search_similar(favourites, total_movies)
+        self.recommendation.append(str(index_results))
+
+    async def get_recommendation(self, favourites):
+        self.search_index(favourites)
+        new_recommendations = await self.httpLayer.prompt_lama(self.recommendation[0])
+        self.recommendation.append(new_recommendations)
+        return self.recommendation
+ 
