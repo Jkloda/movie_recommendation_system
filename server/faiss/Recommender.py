a7ace95cb5cc130f1aa01c7a23fe2f9ccc738db9
Martin Cameron <highlandrocketofficial@gmail.com>
2025-03-04 01:26:57 +0000

    cleaned return data and fixed continually repeated appending data

--- server/faiss/Recommender.py
+++ server/faiss/Recommender.py
@@ -1,6 +1,5 @@
 from Indexer import Indexer
 from Http import HttpLayer
-import asyncio
 
 class Recommender:
     def __init__(self):
@@ -9,13 +8,15 @@ class Recommender:
         self.indexer = Indexer()
 
     def search_index(self, favourites):
+        self.recommendation = []
+        total_movies = len(favourites)
         index_results = self.indexer.search_similar(favourites, total_movies)
         self.recommendation.append(str(index_results))
 
     async def get_recommendation(self, favourites):
-        print(favourites)
         self.search_index(favourites)
         new_recommendations = await self.httpLayer.prompt_lama(self.recommendation[0])
-        self.recommendation.append(new_recommendations)
+        self.recommendation = []
+        self.recommendation.append(new_recommendations.replace("\n", " ").replace("\\", ""))
         return self.recommendation
  
