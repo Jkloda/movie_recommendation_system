c2363088325b7ba4d9ed7e1a437dfba3822a9ebb
JULIA KLODA <19019705@uhi.ac.uk>
2025-04-22 09:48:59 +0100

    changed routes app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,74 @@
 import "./App.css";
-//import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
 import { Login } from "./components/Login.js";
 import { Registration } from "./components/Registration.js";
 import { Searchbar } from "./searchbar.js";
 import { Profile } from "./components/profile/Profile.js";
 import { SemanticSearchBar } from "./components/SemanticSearchBar.js";
+import { HomePage } from "./pages/HomePage.js";
+import { PopularMovies } from "./components/PopularMovies.js";
+
+const PrivateRoute = ({ isAuthenticated, children }) => {
+  return isAuthenticated ? children : <Navigate to="/login" replace />;
+};
 
 export function App() {
-  /* const [data, setData] = useState(null);
-
-useEffect(() => {
-  fetch('http://127.0.0.1:5000/api/data')
-      .then(response => response.json())
-      .then(data => setData(data.message))
-      .catch(error => console.error("Error fetching data:", error));
-}, []); */
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Login" element={<Login />}></Route>
-        <Route path="/Registration" element={<Registration />}></Route>
-        <Route path="/search" element={<Searchbar />}></Route>
-        <Route path="/semanticsearch" element={<SemanticSearchBar />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
+        <Route
+          path="/login"
+          element={<Login onLogin={() => setIsAuthenticated(true)} />}
+        />
+        <Route
+          path="/SignUp"
+          element={<Registration setIsAuthenticated={setIsAuthenticated} />}
+        />
+        <Route
+          path="/Homepage"
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <Searchbar />
+            </PrivateRoute>
+          }
+        />{" "}
+        <Route
+          path="/popularmovies"
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <PopularMovies />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/semanticsearch"
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <SemanticSearchBar />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+        {/* Redirect all unknown paths to login */}
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );
