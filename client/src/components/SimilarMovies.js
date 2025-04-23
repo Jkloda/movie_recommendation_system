267514afbc984f1c7c9e488fc489974f14af7d50
JULIA KLODA <19019705@uhi.ac.uk>
2025-04-23 18:44:55 +0100

    Added poster fetching  using TMDB API

--- client/src/components/SimilarMovies.js
+++ client/src/components/SimilarMovies.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 export const SimilarMovies = () => {
   const [favourites, setFavourites] = useState([]);
   const [similarMovies, setSimilarMovies] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [posters, setPosters] = useState();
 
   useEffect(() => {
     const fetchFavourites = async () => {
@@ -31,6 +33,36 @@ export const SimilarMovies = () => {
 
     fetchFavourites();
   }, []);
+
+  const fetchMoviePoster = async (title) => {
+    try {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${title}`
+      );
+      const data = await res.json();
+      return data.results?.[0]?.poster_path || null;
+    } catch (error) {
+      console.error("Error fetching poster:", error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    async function fetchPosters() {
+      const moviePosters = await Promise.all(
+        similarMovies.map(async (movie) => {
+          let path = await fetchMoviePoster(movie.title);
+          return path;
+        })
+      );
+      return moviePosters;
+    }
+    async function beginFetching() {
+      let poster = await fetchPosters();
+      setPosters(poster);
+    }
+    beginFetching();
+  }, [similarMovies]);
 
   // Step 2: Fetch similar movies
   useEffect(() => {
@@ -84,18 +116,27 @@ export const SimilarMovies = () => {
           gap: "15px",
         }}
       >
-        {similarMovies.map((movie) => (
-          <div
-            key={movie.id}
-            style={{
-              padding: "10px",
-              border: "1px solid #ccc",
-              borderRadius: "8px",
-            }}
-          >
-            <p>{movie.title}</p>
-          </div>
-        ))}
+        {posters
+          ? similarMovies.map((movie, index) => {
+              const poster = posters[index];
+              console.log("poster ", poster);
+              if (poster === null) return null;
+              let path = `https://image.tmdb.org/t/p/w500${poster}`;
+              return (
+                <div
+                  key={movie.id}
+                  style={{
+                    padding: "10px",
+                    border: "1px solid #ccc",
+                    borderRadius: "8px",
+                  }}
+                >
+                  <p>{movie.title}</p>
+                  <img src={path} alt={movie.title} className="movie-img" />
+                </div>
+              );
+            })
+          : null}
       </div>
     </div>
   );
